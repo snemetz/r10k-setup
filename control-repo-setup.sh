99dc470dce44f3e42225b221f8cd1dd6884a13ce
Steven Nemetz <steven.nemetz@fireeye.com>
2015-06-18 11:52:52 -0700

    Add install script

--- control-repo-setup.sh
+++ control-repo-setup.sh
@@ -131,6 +131,15 @@ exec { 'trigger_r10k':
 }
 cDirEnv
 
+cat <<INSTALL >install.sh
+#!/bin/bash
+puppet module install zack/r10k
+puppet apply configure_r10k.pp
+# puppet apply configure_directory_environments.pp
+r10k deploy environment -pv
+INSTALL
+chmod +x install.sh
+
 git add --all
 git commit -m 'Initial creation'
 git branch -m master production
