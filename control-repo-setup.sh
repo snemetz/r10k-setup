51cd0a4eabdf3112a6736022225e150e0cf436f0
Steven Nemetz <steven.nemetz@fireeye.com>
2015-06-18 13:26:25 -0700

    Add copying hiera.yaml to install script

--- control-repo-setup.sh
+++ control-repo-setup.sh
@@ -137,6 +137,10 @@ puppet module install zack/r10k
 puppet apply configure_r10k.pp
 # puppet apply configure_directory_environments.pp
 r10k deploy environment -pv
+# Copy hiera.yaml from production environment to confdir
+hiera=$(puppet config print | grep hiera_config | cut -d= -f2)
+environments=$(puppet config print | grep environmentpath | cut -d= -f2)
+cp ${environments}/production/hiera.yaml ${hiera}
 INSTALL
 chmod +x install.sh
 
